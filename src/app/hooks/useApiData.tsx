6004f7b2ea35d4fb01d484787eb3bfb1a570339c
Thomas Basham <Bashamtg@gmail.com>
2023-12-28 11:10:20 -0800

    updated api route data handling

--- src/app/hooks/useApiData.tsx
+++ src/app/hooks/useApiData.tsx
@@ -1,7 +1,7 @@
-import useSWR from "swr";
+import useSWR, { useSWRConfig } from "swr";
 import { useState, useEffect } from "react";
 
-interface StockedLake {
+export interface StockedLake {
   id: number;
   lake: string;
   stocked_fish: number;
@@ -13,6 +13,10 @@ interface StockedLake {
   longitude: number;
   directions: string;
   derby_participant: boolean;
+}
+
+export interface StockedLakesDataProps {
+  data: StockedLake[];
 }
 
 interface HatcheryTotal {
@@ -33,50 +37,116 @@ interface DateDataUpdated {
 }
 
 export interface DateRange {
-  startDate: Date | null;
-  endDate: Date | null;
+  startDate: string | null;
+  endDate: string | null;
 }
 
-// const route = 'https://washington-trout-stats.vercel.app'
-const route = "http://localhost:5000";
-// const route = "";
-
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
-
 const useApiData = (dateRange: DateRange) => {
+  const { mutate } = useSWRConfig();
+
+  // const route = 'https://washington-trout-stats.vercel.app'
+  const route = "http://localhost:5000";
+  // const route = "";
+
+  const fetcher = (url: string) =>
+    fetch(url).then((res) => {
+      // console.log(res.json())
+      return res.json();
+    });
+
+  const [stockedLakesData, setStockedLakesData] = useState(null);
+  const [hatcheryTotals, setHatcheryTotals] = useState(null);
+  const [totalStockedByDate, setTotalStockedByDate] = useState(null);
+  const [derbyLakesData, setDerbyLakesData] = useState(null);
+  const [dateDataUpdated, setDateDataUpdated] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   const dateQuery = `?start_date=${dateRange.startDate}&end_date=${dateRange.endDate}`;
 
-  const { data: stockedLakesData, error: stockedLakesError } = useSWR(
-    route + "/stocked_lakes_data" + dateQuery,
-    fetcher
-  );
+  // Helper function to handle data fetching
+  const fetchData = async (url: string, setData: (data: any) => void) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setData(data);
+      console.log(data);
+    } catch (error) {
+      setError(error);
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-  const { data: hatcheryTotals, error: hatcheryTotalsError } = useSWR(
-    route + "/hatchery_totals" + dateQuery,
-    fetcher
-  );
+  // Use useSWR for automatic caching and re-fetching
 
-  const { data: totalStockedByDate, error: totalStockedByDateError } = useSWR(
-    route + "/total_stocked_by_date_data" + dateQuery,
-    fetcher
-  );
+  const { data: stockedLakesDataFromApi, isValidating: stockedLakesLoading } =
+    useSWR(route + "/stocked_lakes_data" + dateQuery, fetcher);
 
-  const { data: derbyLakesData, error: derbyLakesError } = useSWR(
-    route + "/derby_lakes_data",
-    fetcher
-  );
+  const { data: hatcheryTotalsFromApi, isValidating: hatcheryTotalsLoading } =
+    useSWR(route + "/hatchery_totals" + dateQuery, fetcher);
 
-  const { data: dateDataUpdated, error: dateDataUpdatedError } = useSWR(
-    route + "/date_data_updated",
-    fetcher
-  );
+  const {
+    data: totalStockedByDateFromApi,
+    isValidating: totalStockedByDateLoading,
+  } = useSWR(route + "/total_stocked_by_date_data" + dateQuery, fetcher);
 
-  const loading =
-    !stockedLakesData ||
-    !hatcheryTotals ||
-    !totalStockedByDate ||
-    !derbyLakesData ||
-    !dateDataUpdated;
+  const { data: derbyLakesDataFromApi, isValidating: derbyLakesLoading } =
+    useSWR(route + "/derby_lakes_data", fetcher);
+
+  const { data: dateDataUpdatedFromApi, isValidating: dateDataUpdatedLoading } =
+    useSWR(route + "/date_data_updated", fetcher);
+
+  useEffect(() => {
+    // Update state when data is fetched
+    if (!stockedLakesLoading && stockedLakesDataFromApi) {
+      setStockedLakesData(stockedLakesDataFromApi);
+    }
+
+    if (!hatcheryTotalsLoading && hatcheryTotalsFromApi) {
+      setHatcheryTotals(hatcheryTotalsFromApi);
+    }
+
+    if (!totalStockedByDateLoading && totalStockedByDateFromApi) {
+      setTotalStockedByDate(totalStockedByDateFromApi);
+    }
+
+    if (!derbyLakesLoading && derbyLakesDataFromApi) {
+      setDerbyLakesData(derbyLakesDataFromApi);
+    }
+
+    if (!dateDataUpdatedLoading && dateDataUpdatedFromApi) {
+      setDateDataUpdated(dateDataUpdatedFromApi);
+    }
+
+    // Update loading state
+    setLoading(
+      stockedLakesLoading ||
+        hatcheryTotalsLoading ||
+        totalStockedByDateLoading ||
+        derbyLakesLoading ||
+        dateDataUpdatedLoading
+    );
+  }, [
+    stockedLakesLoading,
+    stockedLakesDataFromApi,
+    hatcheryTotalsLoading,
+    hatcheryTotalsFromApi,
+    totalStockedByDateLoading,
+    totalStockedByDateFromApi,
+    derbyLakesLoading,
+    derbyLakesDataFromApi,
+    dateDataUpdatedLoading,
+    dateDataUpdatedFromApi,
+  ]);
+
+  const mutateData = (endPoint: string) => {
+    // Manually trigger a re-fetch for stocked lakes data
+    mutate(route + endPoint + dateQuery);
+    console.log("MUTATED");
+    console.log(stockedLakesData);
+  };
 
   return {
     stockedLakesData,
@@ -84,125 +154,23 @@ const useApiData = (dateRange: DateRange) => {
     derbyLakesData,
     totalStockedByDate,
     dateDataUpdated,
+    mutateData,
     loading,
-    error: stockedLakesError || hatcheryTotalsError || totalStockedByDateError || derbyLakesError || dateDataUpdatedError,
+    error,
+    // Expose individual fetch functions
+    fetchStockedLakesData: () =>
+      fetchData(route + "/stocked_lakes_data" + dateQuery, setStockedLakesData),
+    fetchHatcheryTotals: () =>
+      fetchData(route + "/hatchery_totals" + dateQuery, setHatcheryTotals),
+    fetchTotalStockedByDate: () =>
+      fetchData(
+        route + "/total_stocked_by_date_data" + dateQuery,
+        setTotalStockedByDate
+      ),
+    fetchDerbyLakesData: () =>
+      fetchData(route + "/derby_lakes_data", setDerbyLakesData),
+    fetchDateDataUpdated: () =>
+      fetchData(route + "/date_data_updated", setDateDataUpdated),
   };
 };
-
-
 export default useApiData;
-
-
-
-
-// import { useState, useEffect } from "react";
-
-// interface StockedLake {
-//   id: number;
-//   lake: string;
-//   stocked_fish: number;
-//   species: string;
-//   weight: number;
-//   hatchery: string;
-//   date: string;
-//   latitude: number;
-//   longitude: number;
-//   directions: string;
-//   derby_participant: boolean;
-// }
-
-// interface HatcheryTotal {
-//   date: string;
-//   amount: number;
-// }
-
-// interface DerbyLake {
-//   // Define the structure of derby lake data here
-// }
-
-// interface TotalStockedByDate {
-//   // Define the structure of total stocked by date data here
-// }
-
-// interface DateDataUpdated {
-//   // Define the structure of date data updated here
-// }
-
-// export interface DateRange {
-//   startDate: Date | null;
-//   endDate: Date | null;
-// }
-
-// // const route = 'https://washington-trout-stats.vercel.app'
-// const route = "http://localhost:5000";
-// // const route = "";
-// const useApiData = (dateRange: DateRange) => {
-//   const [stockedLakesData, setStockedLakesData] = useState<StockedLake[]>([]);
-//   const [hatcheryTotals, setHatcheryTotals] = useState<HatcheryTotal[]>([]);
-//   const [derbyLakesData, setDerbyLakesData] = useState<DerbyLake[]>([]);
-//   const [totalStockedByDate, setTotalStockedByDate] = useState<
-//     TotalStockedByDate[]
-//   >([]);
-//   const [dateDataUpdated, setDateDataUpdated] =
-//     useState<DateDataUpdated | null>(null);
-//   const [loading, setLoading] = useState(true);
-
-//   const dateQuery = `?start_date=${dateRange.startDate}&end_date=${dateRange.endDate}`;
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const stockedLakesResponse = await fetch(
-//           route + "/stocked_lakes_data" + dateQuery
-//         );
-//         const stockedLakesData: StockedLake[] =
-//           await stockedLakesResponse.json();
-//         setStockedLakesData(stockedLakesData);
-
-//         const hatcheryTotalsResponse = await fetch(
-//           route + "/hatchery_totals" + dateQuery
-//         );
-//         const hatcheryTotals: HatcheryTotal[] =
-//           await hatcheryTotalsResponse.json();
-//         setHatcheryTotals(hatcheryTotals);
-
-//         const totalStockedByDateResponse = await fetch(
-//           route + "/total_stocked_by_date_data" + dateQuery
-//         );
-//         const totalStockedByDate: TotalStockedByDate[] =
-//           await totalStockedByDateResponse.json();
-//         console.log(totalStockedByDate);
-//         setTotalStockedByDate(totalStockedByDate);
-
-//         const derbyLakesResponse = await fetch(route + "/derby_lakes_data");
-//         const derbyLakesData: DerbyLake[] = await derbyLakesResponse.json();
-//         setDerbyLakesData(derbyLakesData);
-
-//         const dateDataUpdatedResponse = await fetch(
-//           route + "/date_data_updated"
-//         );
-//         const dateDataUpdated: DateDataUpdated =
-//           await dateDataUpdatedResponse.json();
-//         setDateDataUpdated(dateDataUpdated);
-
-//         setLoading(false);
-//       } catch (error) {
-//         console.error("Error fetching data:", error);
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   return {
-//     stockedLakesData,
-//     hatcheryTotals,
-//     derbyLakesData,
-//     totalStockedByDate,
-//     dateDataUpdated,
-//     loading,
-//   };
-// };
-
-// export default useApiData;
