adffa894cc75f80309ee78c55211d90ceffecf5d
Thomas Basham <Bashamtg@gmail.com>
2023-12-28 15:17:01 -0800

    updated header nav links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header>
       <nav className="bg-white border-gray-200 ">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="https://flowbite.com/" className="flex items-center">
+          <a href="/" className="flex items-center">
             <Image
               className="w-10 h-8 mr-3"
               width={0}
@@ -48,7 +48,7 @@ export default function Header() {
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white ">
               <li>
                 <a
-                  href="#"
+                  href="/"
                   className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:hover:text-blue-700 "
                   aria-current="page"
                 >
@@ -57,8 +57,8 @@ export default function Header() {
               </li>
               <li>
                 <a
-                  href="#"
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 "
+                  href="/about"
+                  className="md:hover:text-blue-700 block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 "
                 >
                   About
                 </a>
