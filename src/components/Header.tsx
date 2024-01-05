500be4091c8037d6d7fd004df39af6e7673d7ef7
Thomas Basham <Bashamtg@gmail.com>
2024-01-04 16:31:04 -0800

    updated nav for about and contact page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -66,7 +66,7 @@ export default function Header() {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/contact"
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
                 >
                   Contact
