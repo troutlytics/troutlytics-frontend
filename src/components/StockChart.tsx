ed5f42b1e20de30269ea6ba419b65658dc8a5039
Thomas Basham <Bashamtg@gmail.com>
2024-01-04 16:30:19 -0800

    cleaned up stock chart code

--- src/components/StockChart.tsx
+++ src/components/StockChart.tsx
@@ -1,16 +1,16 @@
 // components/StockChart.tsx
-import React from 'react';
-import { Line } from 'react-chartjs-2';
-import Chart from 'chart.js/auto';
+import React from "react";
+import { Line } from "react-chartjs-2";
+import Chart from "chart.js/auto";
 interface StockChartProps {
-  lakes: [string, number][];
-  loading: boolean
+  data: [string, number][];
+  loading: boolean;
 }
 
-const StockChart: React.FC<StockChartProps> = ({ lakes }) => {
+const StockChart: React.FC<StockChartProps> = ({ data }) => {
   Chart.register();
-  if (lakes && lakes.length > 0) {
-    const [dates, totalStockedFish] = lakes.reduce(
+  if (data && data.length > 0) {
+    const [dates, totalStockedFish] = data.reduce(
       (acc, [date, totalStocked]) => {
         acc[0].push(date);
         acc[1].push(totalStocked);
@@ -23,10 +23,10 @@ const StockChart: React.FC<StockChartProps> = ({ lakes }) => {
       labels: dates,
       datasets: [
         {
-          label: 'Amount Stocked',
+          label: "Amount Stocked",
           data: totalStockedFish,
-          backgroundColor: '#9fd3c7',
-          borderColor: '#9fd3c7',
+          backgroundColor: "#9fd3c7",
+          borderColor: "#9fd3c7",
           borderWidth: 1,
           pointRadius: 2,
         },
@@ -36,18 +36,20 @@ const StockChart: React.FC<StockChartProps> = ({ lakes }) => {
     const chartOptions = {
       scales: {
         y: {
-          ticks: { color: '#ececec', beginAtZero: true },
+          ticks: { color: "#ececec", beginAtZero: true },
         },
         x: {
-          ticks: { color: '#ececec' },
+          ticks: { color: "#ececec" },
         },
       },
     };
 
     return (
-      <div className='w-full'>
-        <h2 className='lg:text-5xl md:text-4xl sm:text-2xl'>Total Stocked in Washington by Date</h2>
-        <Line className='' data={chartData}  /> {/* options={chartOptions}  */}
+      <div className="w-full">
+        <h2 className="lg:text-5xl md:text-4xl sm:text-2xl">
+          Total Stocked in Washington by Date
+        </h2>
+        <Line className="" data={chartData} /> {/* options={chartOptions}  */}
       </div>
     );
   } else {
