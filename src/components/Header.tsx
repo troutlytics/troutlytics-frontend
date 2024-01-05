e5ce9d096f1c2fe750c5a9814b2ab0661f6f3709
Thomas Basham <Bashamtg@gmail.com>
2024-01-04 16:36:06 -0800

    changed a to Link for nextjs purposes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -65,12 +65,12 @@ export default function Header() {
                 </Link>
               </li>
               <li>
-                <a
+                <Link
                   href="/contact"
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
                 >
                   Contact
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
