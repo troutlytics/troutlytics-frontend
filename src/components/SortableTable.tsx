e369101c0903d217291b1c21691cfee67457e26c
Thomas Basham <Bashamtg@gmail.com>
2024-01-24 13:18:11 -0800

    refactored js for code readability

--- src/components/SortableTable.tsx
+++ src/components/SortableTable.tsx
@@ -12,7 +12,7 @@ const SortableTable: React.FC<SortableTableProps> = ({
   loading,
 }) => {
   const [sortedData, setSortedData] = useState<StockedLake[] | []>([]);
-  const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
+  const [sortDirection, setSortDirection] = useState<"asc" | "desc">("desc");
   const [sortedColumn, setSortedColumn] = useState<string | null>(null);
 
   useEffect(() => {
@@ -23,15 +23,14 @@ const SortableTable: React.FC<SortableTableProps> = ({
 
   const sortData = (field: keyof StockedLake) => {
     const newData = [...sortedData];
-    console.log(newData)
     newData.sort((a, b) => {
       if (field === "date") {
         // Parse dates and compare
         const dateA = new Date(a[field]);
         const dateB = new Date(b[field]);
         return sortDirection === "desc"
-          ? dateA.getTime() - dateB.getTime()
-          : dateB.getTime() - dateA.getTime();
+          ? dateA.getDate() - dateB.getDate()
+          : dateB.getDate() - dateA.getDate();
       } else {
         // Existing logic for other fields
         if (a[field] < b[field]) {
@@ -57,46 +56,81 @@ const SortableTable: React.FC<SortableTableProps> = ({
       : "";
   };
 
+  const thNames = {
+    date: "date",
+    lake: "lake",
+    hatchery: "hatchery",
+    species: "species",
+    stocked_fish: "amount produced",
+    weight: "fish per pound",
+    directions: "directions",
+  };
+
+  const HeaderRow = () => {
+    return (
+      <tr>
+        {Object.keys(thNames).map((key) => (
+          <th
+            key={key}
+            className="px-6 py-3 text-left text-xs font-medium text-gray-200 uppercase tracking-wider cursor-pointer hover:text-gray-400"
+            onClick={() => sortData(key as keyof StockedLake)}
+          >
+            {/* @ts-ignore */}
+            {thNames[key].replaceAll("_", " ")}
+            {renderSortIcon(key)}
+          </th>
+        ))}
+      </tr>
+    );
+  };
+
+  // Skeleton Row for loading state
+  const SkeletonRow = () => (
+    <tr>
+      {/* Adjust the number of cells to match your table's column count */}
+      {Object.values(thNames).map((_, index) => (
+        <td
+          key={index}
+          className="px-6 py-4 whitespace-nowrap text-sm text-gray-300 animate-pulse"
+        >
+          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-48 mb-4"></div>
+        </td>
+      ))}
+    </tr>
+  );
+
+  const SkeletonBody = () => {
+    return (
+      <>
+        <SkeletonRow />
+        <SkeletonRow />
+        <SkeletonRow />
+        <SkeletonRow />
+        <SkeletonRow />
+        <SkeletonRow />
+      </>
+    );
+  };
+
   return (
     <div className="max-h-96 overflow-auto w-full">
       <table className="min-w-full divide-y divide-gray-300 sticky">
         <thead className="sticky top-0 bg-gray-700">
-          <tr>
-            {[
-              "date",
-              "hatchery",
-              "lake",
-              "species",
-              "stocked_fish",
-              "weight",
-            ].map((field) => (
-              <th
-                key={field}
-                className="px-6 py-3 text-left text-xs font-medium text-gray-200 uppercase tracking-wider cursor-pointer hover:text-gray-400"
-                onClick={() => sortData(field as keyof StockedLake)}
-              >
-                {field === 'weight' ? "Fish Per Pound" : field.replaceAll("_", " ")}
-                {renderSortIcon(field)}
-              </th>
-            ))}
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-200 uppercase tracking-wider">
-              Directions
-            </th>
-          </tr>
+          <HeaderRow />
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
+          {(loading || data.length == 0) && <SkeletonBody />}
           {sortedData &&
             sortedData.map((lake, index) => (
               <tr key={index}>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                  {/* {lake.date} */}
                   {formatDate(lake.date)}
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                  {lake.hatchery}
+                  {lake.lake}
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                  {lake.lake}
+                  {lake.hatchery}
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
                   {lake.species}
