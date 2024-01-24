5d4eb26e6d7e267092f71d4ca8f82a90c91c3035
Thomas Basham <Bashamtg@gmail.com>
2024-01-24 13:42:40 -0800

    added date data updated to footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,17 +1,29 @@
 // import type { Metadata } from "next";
 import Header from "./Header";
-
+import useApiData from "@/hooks/useApiData";
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { dateDataUpdated } = useApiData({
+    startDate: new Date().toISOString(),
+    endDate: new Date().toISOString(),
+  });
   return (
     <>
       <Header />
       <main className="main mx-auto  flex min-h-screen flex-col items-center justify-between lg:p-24 ">
         {children}
       </main>
+      <footer>
+        <p className="text-center mb-10">
+          Data was last updated {dateDataUpdated} from the{" "}
+          <a href="https://wdfw.wa.gov/fishing/reports/stocking/trout-plants/all">
+            WDFW trout plant reports page
+          </a>
+        </p>
+      </footer>
     </>
   );
 }
