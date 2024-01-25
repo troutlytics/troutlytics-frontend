4900db36d5459697f13c36f18ef7c16ed711c5d9
Thomas Basham <Bashamtg@gmail.com>
2024-01-25 11:04:21 -0800

    updated footer date format

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,7 @@
 // import type { Metadata } from "next";
 import Header from "./Header";
 import useApiData from "@/hooks/useApiData";
+import { formatDate } from "@/pages";
 export default function RootLayout({
   children,
 }: {
@@ -18,8 +19,13 @@ export default function RootLayout({
       </main>
       <footer>
         <p className="text-center mb-10">
-          Data was last collected on {dateDataUpdated} from the{" "}
-          <a className=" text-cyan-700 hover:text-cyan-900" href="https://wdfw.wa.gov/fishing/reports/stocking/trout-plants/all">
+          Data was last collected on {formatDate(dateDataUpdated)} from the{" "}
+          <a
+            className=" text-cyan-700 hover:text-cyan-900"
+            href="https://wdfw.wa.gov/fishing/reports/stocking/trout-plants/all"
+            target="_blank"
+            rel="noreferrer nofollow"
+          >
             WDFW trout plant reports page
           </a>
           .
