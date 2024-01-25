af65356bbae3cb67d10f3a5e33751ffa2ce11267
Thomas Basham <Bashamtg@gmail.com>
2024-01-25 13:27:02 -0800

    added context layer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import Layout from "../components/layout";
 import type { AppProps } from "next/app";
+import { ApiDataProvider } from "@/contexts/DataContext";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ApiDataProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApiDataProvider>
   );
 }
