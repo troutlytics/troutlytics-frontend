ea7d7ed781dfbbb9e5a7f107f92dc78e7674c16d
Thomas Basham <Bashamtg@gmail.com>
2025-06-16 19:13:42 -0700

    switched date formatting from utc time to local for better ux

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,11 @@
 // Helper Function to format a date in a readable way
 export const formatDate = (dateStr: string | undefined | null) => {
   if (dateStr) {
-    // Create a date object in UTC
+    // Create a date object in Local time
     const date = new Date(dateStr);
-    const year = date.getUTCFullYear();
-    const month = String(date.getUTCMonth() + 1).padStart(2, "0"); // months are 0-indexed
-    const day = String(date.getUTCDate()).padStart(2, "0");
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // months are 0-indexed
+    const day = String(date.getDate()).padStart(2, "0");
 
     return `${month}/${day}/${year}`;
   } else return "";
@@ -18,4 +18,5 @@ export const calculateDate = (days: number) => {
 };
 
 // helper to get “2025-05-22” style dates
-export const isoFormat = (d: Date | null | undefined) => d?.toISOString().split("T")[0];
+export const isoFormat = (d: Date | null | undefined) =>
+  d?.toISOString().split("T")[0];
